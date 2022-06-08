377450c5ecf56c191779a16d036de6a23adfe6af
木荣 <372638156@qq.com>
2022-06-08 14:57:03 +0800

    feat: cnpn

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# cnpn
+
+<p align="center">
+  <h1  align="center">cnpn</h1>
+</p>
+
+<p align="center">
+I want to create a npm package. I always have to visit <code>https://www.npmjs.com/</code> when I check package name is registered! And it may not be possible to check. The experience was troublesome and bad. So I publish <code>cnpn</code> tools help me check package name is registered. 😄
+</p>
 
 [![NPM version](https://img.shields.io/npm/v/cnpn?color=a1b858&label=)](https://www.npmjs.com/package/cnpn)
+
+## 🚀 Usage 
+
+```shell
+$ npm i -g cnpn
+$ cnpn <your package name>
+```
+
+### e.g.
+
+```shell
+$ cnpn cnpn
+> 😫 This package name is register already!
+$ cnpn cnpn2
+> 😊 You can use this package name!
+```
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-export const one = 1
-export const two = 2
+import axios from 'axios'
+import consola from 'consola'
+const BASE_URL = 'https://registry.npmjs.org/'
+
+if (process.argv.length > 2) {
+  const args = process.argv.slice(2)
+  const packageName = args[0]
+  const url = `${BASE_URL}${packageName}`
+  axios.get(url).then((res) => {
+    if (res.status === 200) {
+      if (res.data.name)
+        consola.warn('😫 This package name is register already!')
+      else
+        consola.success('😊 You can use this package name!')
+    }
+  }).catch((err) => {
+    if (err.response.status === 404 && err.response.statusText === 'Not Found')
+      consola.log('😊 You can use this package name!')
+    else
+      consola.success(`😫 ${err}`)
+  })
+}
+else {
+  consola.warn('😫 Please input package name for you will check!')
+}
