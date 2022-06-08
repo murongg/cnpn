73f37390680fca451bbeffe91e66395cd74da39d
木荣 <372638156@qq.com>
2022-06-08 16:03:16 +0800

    feat: loading

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import axios from 'axios'
 import consola from 'consola'
+import ora from 'ora'
+
 const BASE_URL = 'https://registry.npmjs.org/'
+const loading = ora('checking').start()
 
 if (process.argv.length > 2) {
   const args = process.argv.slice(2)
   const packageName = args[0]
   const url = `${BASE_URL}${packageName}`
   axios.get(url).then((res) => {
+    loading.stop()
     if (res.status === 200) {
       if (res.data.name)
         consola.warn('😫 This package name is register already!')
@@ -14,6 +18,7 @@ if (process.argv.length > 2) {
         consola.success('😊 You can use this package name!')
     }
   }).catch((err) => {
+    loading.stop()
     if (err.response && err.response.status === 404 && err.response.statusText === 'Not Found')
       consola.log('😊 You can use this package name!')
     else
@@ -21,5 +26,6 @@ if (process.argv.length > 2) {
   })
 }
 else {
+  loading.clear()
   consola.warn('😫 Please input package name for you will check!')
 }
